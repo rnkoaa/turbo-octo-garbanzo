d0c1f0dc133076bfdc8a5c1f52dcc15752f1fc1b
Richard Agyei <richard.agyei@gmail.com>
2021-11-11 11:41:56 -0600

    Product OpenAPI

--- src/main/java/com/richard/Product.java
+++ src/main/java/com/richard/Product.java
@@ -1,7 +1,9 @@
 package com.richard;
 
 import io.micronaut.core.annotation.Introspected;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.UUID;
 
 @Introspected
+@Schema(name = "Product", description = "Product schema")
 public record Product(UUID id, String name, ProductVariant variant) {}
--- src/main/java/com/richard/ProductVariant.java
+++ src/main/java/com/richard/ProductVariant.java
@@ -2,8 +2,10 @@ package com.richard;
 
 import io.micronaut.core.annotation.Introspected;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.UUID;
 
 @Introspected
+@Schema(name = "ProductVariant", description = "Product Variant schema")
 public record ProductVariant (UUID id, String name){
 }
